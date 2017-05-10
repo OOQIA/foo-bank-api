b84bddd625e2081f7da7f8195e33ef1ec1402b7d
LRPalacios <lrpalaciosdev@gmail.com>
2017-05-10 14:00:51 -0600

    add support for method not allowed

--- src/routes/customerRoute.js
+++ src/routes/customerRoute.js
@@ -1,17 +1,21 @@
 import express from 'express';
 import customers from '../models/customer';
 import CustomerController from '../controllers/customerConstroller';
+import { methodNotAllowedHandler } from '../utils/method-not-allowed-handler';
 
 export default (db) => {
   const router = express.Router();
   const controller = new CustomerController(customers(db));
 
   router.route('/users')
-    .post((req, res) => controller.post(req, res));
+    .post((req, res) => controller.post(req, res))
+    .all(methodNotAllowedHandler);
 
   router.route('/users/:user_id')
     .get((req, res) => controller.get(req, res))
-    .put((req, res) => controller.put(req, res));
+    .put((req, res) => controller.put(req, res))
+    .all(methodNotAllowedHandler);
 
   return router;
 };
+
