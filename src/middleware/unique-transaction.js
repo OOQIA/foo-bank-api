7a9b86f65e9fadb6b52dfb7447a7e8cd1a8966b3
Sergio Ruiz <sergio.ruiz@ooqia.com>
2017-05-12 11:37:40 -0600

    Added check duplicated transaction id check to middleware

--- src/middleware/unique-transaction.js
+++ src/middleware/unique-transaction.js
@@ -1,11 +1,20 @@
 import { badRequest } from '../utils/action-result';
 import { MISSING_UNIQUE_INTERNAL, MISSING_UNIQUE_MESSAGE } from './unique-transaction-messages';
+import Transaction from '../models/transaction';
 
-export default ({ db }) => (req, res, next) => {
+export default (db) => (req, res, next) => {
   const transactionId = req.get('X-Unique-Transaction-ID');
   if (transactionId) {
-    // TODO: Check if transaction Id is actually valid
-    next();
+    const transactions = Transaction(db);
+    transactions
+      .findOne({ where: { x_unique_transaction_id: req.get('X-Unique-Transaction-ID') } })
+      .then((xTransaction) => {
+        if (xTransaction) {
+          res.json({ error: 'Duplicated Transaction Id' });
+          return;
+        }
+        next();
+      });
     return;
   }
   badRequest(res, MISSING_UNIQUE_MESSAGE, MISSING_UNIQUE_INTERNAL);
